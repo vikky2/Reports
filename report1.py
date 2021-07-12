a46275a58c5900852b4cf18b803219e143d552c9
Vikky2 <vikky.ranjan2@gmail.com>
2021-07-12 10:26:03 +0530

    some code added and both test cases passed

---
+++ report1.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import pytest
+
+class TestOrangeHRM():
+
+    @pytest.fixture()
+    def setup(self):
+        self.driver=webdriver.Chrome()
+        self.driver.maximize_window()
+        yield
+        self.driver.close()
+
+    def test_homepageTitle(self, setup):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        assert self.driver.title=="OrangeHRM"
+
+    def test_login(self, setup):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+
+        assert self.driver.title=="OrangeHRM"
+
+
+
+
+
